c145d24e11d0aa8dd7b2ae33b18e7d0226f43c5e
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-10-05 02:43:34 -0300

    feat(animations): init AOS library

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 // Lógica de Funcionalidades em um Arquivo Único
+
+// Inicializando as Animações de Scroll
+AOS.init(); // Import do AOS
+AOS.init({
+  disable: false, 
+  startEvent: 'DOMContentLoaded', 
+  initClassName: 'aos-init', 
+  animatedClassName: 'aos-animate', 
+  useClassNames: false,
+  disableMutationObserver: false, 
+  debounceDelay: 50, 
+  throttleDelay: 99, 
+  
+  offset: 120,
+  delay: 0, 
+  duration: 400, 
+  easing: 'ease', 
+  once: false, 
+  mirror: false, 
+  anchorPlacement: 'top-bottom', 
+});
+// Fim da Inicialização do AOS
+
+// Ativação de Links nos Cards
+
+// Fim da Ativação de Links
