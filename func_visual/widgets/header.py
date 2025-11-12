bfda6c6aa16eddc7ecf2e58d1317fef0c824e571
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-11-11 22:41:21 -0300

    refactor(ui): standardize header close button

--- func_visual/widgets/header.py
+++ func_visual/widgets/header.py
@@ -5,5 +5,5 @@ def nav(self, controller, texto):
     header.place(relx=0, rely=0, relwidth=1, relheight=0.1)
     titulo = CTkLabel(header, text=texto, font=("Bold", 20), text_color="#E6C8FA")
     titulo.place(relx=0.1, rely=0.5, anchor=CENTER)
-    close = CTkButton(header, text="X", font=("Bold", 20), text_color="#E6C8FA", command=controller.destroy, fg_color="red")
+    close = CTkButton(header, text="X", font=("Bold", 20), text_color="#E6C8FA", command=controller.destroy, fg_color="#E3242B", hover_color="#A1171B")
     close.place(relx=0.95, rely=0.5, anchor=CENTER, relwidth=0.05, relheight=0.5)
