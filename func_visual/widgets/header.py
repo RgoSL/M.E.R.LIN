3e1e12c9c9de57960db25f48c47fdfe98eb76362
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-11-12 00:03:24 -0300

    feat(ui): add new logo and apply in header

--- func_visual/widgets/header.py
+++ func_visual/widgets/header.py
@@ -1,9 +1,20 @@
-from customtkinter import CTkFrame, CTkLabel, CENTER,CTkButton
+from customtkinter import CTkFrame, CTkLabel, CENTER, CTkButton, CTkImage
+from PIL import Image
 
-def nav(self, controller, texto):
+def nav(self, controller, texto, logo_path=None):
+    # Header principal
     header = CTkFrame(self, fg_color="#654E82", corner_radius=0)
     header.place(relx=0, rely=0, relwidth=1, relheight=0.1)
+    
+    image = CTkImage(Image.open("assets/Logos/NovaLogo.png"), size=(100, 90))
+    logo_label = CTkLabel(header, image=image, text="")
+    logo_label.place(relx=0.05, rely=0.5, anchor=CENTER)
+
+    # Título ao lado da logo
     titulo = CTkLabel(header, text=texto, font=("Bold", 20), text_color="#E6C8FA")
-    titulo.place(relx=0.1, rely=0.5, anchor=CENTER)
-    close = CTkButton(header, text="X", font=("Bold", 20), text_color="#E6C8FA", command=controller.destroy, fg_color="#48365E", hover_color="#332346")
+    titulo.place(relx=0.15, rely=0.5, anchor=CENTER) 
+
+    # Botão de fechar
+    close = CTkButton(header, text="X", font=("Bold", 20), text_color="#E6C8FA", 
+                      command=controller.destroy, fg_color="#48365E", hover_color="#332346")
     close.place(relx=0.95, rely=0.5, anchor=CENTER, relwidth=0.05, relheight=0.5)
